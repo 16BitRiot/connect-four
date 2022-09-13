f5a3c58fc60f2e8226593b730e9add6234e9a201
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2022-09-12 23:23:07 -0400

    competled GameBoard html element ToDO

--- scratch2.js
+++ scratch2.js
@@ -1,9 +1,48 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+ const WIDTH = 7;
+ const HEIGHT = 6;
+ let currPlayer = 1; // active player: 1 or 2
+ let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+//  new vars
+let x = 0;
+let y = 0;
+const emptyColumn = Array.apply(null, Array(WIDTH));
+let yAxis = emptyColumn;
+
+// make yaxis
+const makeYaxis = function(){
+  yAxis.forEach(function(el, id) {y = id; yAxis[id] = [y, x];})
+}
+
+ /** makeBoard: create in-JS board structure:
+  *    board = array of rows, each row is array of cells  (board[y][x])
+  */
+ 
+ function makeBoard() {
+   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+
+   // Run yAxis creator
+   makeYaxis();
+
+  //  Set board ** NOTE ** Only x numbers in the matrix are currently set
+   const setBoard = [...Array(HEIGHT)].forEach((element, i) => {board.push(yAxis)});
+    return board;
+ }
+
 /** makeHtmlBoard: make HTML table and row of column tops. */
 // ******WORKSHEET********  
 
 function makeHtmlBoard() {
     // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-  
+const gameBoard = document.getElementById(`board`);
+
     // TODO: add comment for this code
     var top = document.createElement("tr");
     top.setAttribute("id", "column-top");
