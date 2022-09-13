498ffb0b648492d38f38275c8868d44563da9e7f
Philip Fryer <109255121+16BitRiot@users.noreply.github.com>
2022-09-12 22:23:09 -0400

    Initial commit

---
+++ scratch.js
@@ -0,0 +1,67 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+ const WIDTH = 7;
+ const HEIGHT = 6;
+ 
+ let currPlayer = 1; // active player: 1 or 2
+ let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+//  new vars
+let x = 0;
+let y = 0;
+let yXline = [];
+let yX = [y, x];
+
+const emptyColumn = Array.apply(null, Array(HEIGHT));
+let yAxis = emptyColumn;
+
+// make yaxis
+const makeYaxis = function(){
+  yAxis.forEach(function(el, id) {y = id; yAxis[id] = [y, x];
+  })
+}
+
+
+// function in progress:inserts 1 y/x array in index 0-5 in Yxline
+// can be used if width can be created w/ one variable (replace yX here)
+function tester() {yXline = [...Array(HEIGHT)].fill(yAxis, 0);}
+
+ /** makeBoard: create in-JS board structure:
+  *    board = array of rows, each row is array of cells  (board[y][x])
+  */
+ 
+ function makeBoard() {
+   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+
+   // create height: push 6 arrays w/ [y, x]to board  
+   makeYaxis();
+   const setBoard = [...Array(WIDTH)].forEach((element, i) => {board.push(yAxis)});
+    console.log(board);
+    //  board.forEach((element, id) => {console.log(element); console.log(id)});
+
+ }
+
+
+
+
+
+// *****************************************************************
+// LEFTOVERS/mAY USE OR NEED LATER
+
+// function in progress:inserts 7 y/x arrays in index 0-5 in Yxline
+// function tester() {yXline.map(x => x = yAxis)};
+
+//  sanitized function
+// let rowBlock = [[y, x], [y, x], [y, x], [y, x], [y, x], [y, x], [y, x]];
+// function makeBoard() { 
+//     const setBoard = [...Array(HEIGHT)].forEach((element, i) => board.push(rowBlock));
+//     console.log(board);
+//  }
+// let rowBlock = [[], [], [], [], [], []];mak
+//  }
+// *****************************************************************
